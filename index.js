3ec44dbe561154b708e2f8dbb1a8a4ddb1a636d4
ch2isk4kos <ch2isk4kos@gmail.com>
2019-09-18 16:31:21 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+// constructor
+function BoardMember(name, homeState, training) {
+    this.name = name;
+    this.homeState = homeState;
+    this.training = training;
+
+    //  BoardMember.method()'s
+
+    this.veto = function() {
+        return "No, I must disagree";
+    }
+
+    this.approve = function() {
+        return "You can do that!"
+    }
+
+    this.doCharity = function() {
+        return "I like to help people."
+    }
+
+    this.releasePressStatement = function() {
+        return "You will see great things from Scuber."
+    }
+
+    this.sayHi = function() {
+        return `Hi, my name is ${name}. I am from ${homeState}, and I was trained in ${this.training}.`
+    }
+}
